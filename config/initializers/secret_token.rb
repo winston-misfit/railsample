f9bb30fc2d6db74c01c6e355bb4cd64a3cfbabc8
Winston <winston@Tungs-MacBook-Air.local>
2013-04-09 14:44:14 +0800

    add rails app for chef testing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsample::Application.config.secret_token = '855f4bf96c837a3842df7be1f998f887c4bc26a1f5ec2df95950ddbc97211b711f52bf3ecdd4fc2d5dd7e314c9af5fbfc30e60d5cd71bf102ade54b13cbb5eb1'
